181450c948e985b715d878b585d0d00dffe58e27
studiozut <studiozut@gmail.com>
2015-05-26 11:04:53 -0400

    added node.js and yoeman

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularD8App
+ * @description
+ * # angularD8App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularD8App', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
